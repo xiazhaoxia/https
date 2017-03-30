bd21f4a212af53e46124cf11263a1e82dc2a62cc
suxia <suxia@suxias-mac-mini.local>
2017-03-30 17:13:35 +0800

    Initial Commit

---
+++ browsercertificate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  browsercertificate
+//
+//  Created by suxia on 3/30/17.
+//  Copyright © 2017 suxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
